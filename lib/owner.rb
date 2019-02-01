b2619b14a1b2e83d1e77ec1723326807b5de5e6b
Leonardo Peralta <leo.pm15@gmail.com>
2019-02-01 04:02:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
   # code goes here
+
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = species
+
+
+    @@all << self
+  end
+
+  def self.all
+  @@all
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def name=(name)
+    @name = name
+  end
+
 end
